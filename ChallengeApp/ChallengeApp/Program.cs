993b6d9929202cbd9be5c556bfd6efe625f3d443
Hanka888 <malenstwo.sa@interia.pl>
2023-02-11 21:12:10 +0100

    zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// dzień 4 zadanie domowe 
+var name = "Ewa";
+var age = 24;
+bool female = true;
+
+if (female && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa/ lat 33");
+}
+else if (!female && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else if (female && age > 30)
+{
+    Console.WriteLine("Kobieta powyżej 30 lat");
+}
+else
+{
+    Console.WriteLine("Inna osoba");
+}
+
